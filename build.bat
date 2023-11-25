a0f9c95aca0d09ea371b706c810c3eb1d30303f8
Hydley F <hydley.jailsonk@gmail.com>
2023-11-25 09:26:48 -0300

    Fixed build process

--- build.bat
+++ build.bat
@@ -1,27 +1,29 @@
 @echo off
 chcp 65001
 
-set love_path_64="C:\Program Files\LOVE\"
-set love_path_86="C:\Program Files (x86)\LOVE"
+:: Prepare the project root folder
+mkdir tmp & rmdir /s /q build
+
+:: Build
+mkdir build
+set love_path_x64="C:\Program Files\LOVE\"
+set love_path_x86="C:\Program Files (x86)\LOVE"
+if not exist %love_path_x64% echo Love framework (x64) is non-existent. It was not installed or is in a different path. & exit 1
+if not exist %love_path_x86% echo Love framework (x86) is non-existent. It was not installed or is in a different path. & exit 1
 set executable=não-é-um-vírus
 
-if not exist tmp mkdir tmp
-if not exist build mkdir build
-if not exist build\x64 mkdir build\x64
-if not exist build\x32 mkdir build\x32
+mkdir build\x64 & mkdir build\x86
 
 powershell Compress-Archive -Path src\* -DestinationPath tmp\out.zip
-cd tmp
-ren *.zip *.love
-cd ..
+cd tmp && ren *.zip *.love && cd ..
 
-copy /b %love_path_64%love.exe+"tmp\out.love" build\x64\%executable%.exe
-copy build\x64\%executable%.exe build\x32\%executable%.exe
+copy /b %love_path_x64%love.exe + "tmp\out.love" build\x64\%executable%.exe
+copy build\x64\%executable%.exe build\x86\%executable%.exe
 
-for /R %love_path_64% %%f in (*.dll) do copy "%%f" "build\x64\"
-for /R %love_path_86% %%f in (*.dll) do copy "%%f" "build\x32\"
+for /R %love_path_x64% %%f in (*.dll) do copy "%%f" "build\x64\"
+for /R %love_path_x86% %%f in (*.dll) do copy "%%f" "build\x86\"
+
+:: Apply resources
 
 :: Cleanup
-rmdir /s /q tmp 
-
-build\x64\%executable%.exe
+rmdir /s /q tmp
