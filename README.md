a16e57f2b0936b3c016ff932530fa4f2ea606d90
Hydley F <hydley.jailsonk@gmail.com>
2023-11-25 23:56:16 -0300

    Finish startup window implementation

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # Não é um Vírus
 
-## Building and Development
+## Visão geral
+
+## Execução
+### Requisitos
+O principal requisito para executar o programa é ter um sistema compatível com OpenGL 2.1 ou OpenGL ES 2, bem como os drivers gráficos relevantes. Em todas as três principais plataformas de desktop (Linux, macOS e Windows), o OpenGL geralmente vem integrado ao sistema. No entanto, você precisará baixar e instalar um driver recente para seu hardware gráfico caso ocorram erros de inicialização.
+
+Para instalar os drivers necessários (bem como verificar se o seu sistema é compatível) consulte a [página oficial de download do OpenGL](https://www.khronos.org/opengl/wiki/Getting_Started#Downloading_OpenGL).
+
+### Passos
+Depois de verificar todos os requisitos, basta clicar duas vezes no executável `not-a-virus.exe`. Se tudo correr bem, o programa será executado com sucesso.
+
+## Compilação a partir do código-fonte
 ### Windows
-Se você estiver no Windows, as coisas serão muito mais fáceis, pois existe um arquivo em lote especial para construir o projeto: make.bat.
+Se você estiver no Windows, as coisas são muito mais simples, já que existe um arquivo em lote especial para construir o projeto: [make.bat](https://github.com/Hylley/nao-e-um-virus/blob/main/make.bat).
 
 Existem apenas quatro variações diferentes que este programa aceita como argumentos:
 ```
@@ -17,14 +28,14 @@ make run          :: Executa o projeto sem compilar
 ```
 make build run    :: Compila o projeto e executa o resultado
 ```
-Por padrão os resultados da compilação são armazenados numa pasta chamada "build" que obedece o esquema:
+Por padrão os resultados da compilação são armazenados numa pasta chamada especial que obedece o esquema:
 ```
 .
-├── build/
-│   ├── x64/		# Versão do programa para computadores de 64 bits
-│   └── x86/		# Versão do programa para computadores de 32 bits
+├── build
+│   ├── x64		:: Versão do programa para computadores operando em 64 bits
+│   └── x86		:: Versão do programa para computadores operando em 32 bits
 └── ...
 ```
 
-### Other
-Definitivamente existe uma maneira de construir o programa para outros sistemas operacionais, como Linux. Mas isso provavelmente envolve vários comandos enigmáticos na linha de comando. Por enquanto, você pode acessar a [documentação oficial do Love2D aqui](https://love2d.org/wiki/Game_Distribution) e reunir informações úteis para fazer sua própria automação de build.
+### Outros
+Definitivamente existe uma maneira de construir o programa para outros sistemas operacionais, como Linux. Mas isso provavelmente envolve vários comandos enigmáticos na linha de comando. Por enquanto, você pode acessar a [documentação oficial do Love2D aqui](https://love2d.org/wiki/Game_Distribution) e reunir informações úteis para fazer sua própria automação de build para o seu SO em particular.
--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,82 @@
+local admin_dialog
+local no_button_position = {}
+local close_button
+no_button_position[0] = 330
+no_button_position[1] = 370
+
+function love.load()
+	love.window.setTitle('Controle de Conta de Usuário')
+	love.window.setIcon(love.image.newImageData("assets/images/icons/defender.png"))
+	admin_dialog = love.graphics.newImage("assets/images/admin_dialog_background.png")
+	love.window.setMode(admin_dialog:getWidth(), admin_dialog:getHeight(), {resizable = false, borderless = true})
+
+	close_button = love.graphics.newImage("assets/images/icons/close.png")
+end
+
+
 function love.draw()
-    love.graphics.print("Eu souaa lindo", 0, 0)
+	-- Draw background
+	for i = 0, love.graphics.getWidth() / admin_dialog:getWidth() do
+		for j = 0, love.graphics.getHeight() / admin_dialog:getHeight() do
+			love.graphics.draw(admin_dialog, i * admin_dialog:getWidth(), j * admin_dialog:getHeight())
+		end
+	end
+
+	-- Draw texts
+	love.graphics.setNewFont("assets/fonts/Segoe UI.ttf", 16)
+	love.graphics.print({{0, 0, 0, 1}, "Controle de Conta de Usuário"}, 36, 17)
+
+	love.graphics.setNewFont("assets/fonts/Segoe UI.ttf", 25)
+	love.graphics.print({{0, 0, 0, 1}, "Deseja permitir que este aplicativo de um\nfornecedor desconhecido faça alterações no\nseu dispositivo?"}, 36, 56)
+	love.graphics.print({{0, 0, 0, 1}, "não-é-um-vírus.exe"}, 36, 200)
+
+	love.graphics.setNewFont("assets/fonts/Segoe UI.ttf", 18)
+	love.graphics.print({{0, 0, 0, 1}, "Fornecedor: Desconhecido"}, 36, 260)
+	love.graphics.print({{0, 0, 0, 1}, "Origem do arquivo: Disco rígido deste computador"}, 36, 280)
+
+	love.graphics.print({{0, .4, .8, 1}, "Mostrar mais detalhes"}, 36, 325)
+
+	-- Draw buttons
+	love.graphics.draw(close_button, 609, 20, 0, .07, .07)
+
+	love.graphics.setColor(.7, .7, .7)
+	love.graphics.rectangle("fill", 36, 370, 288, 50)
+	love.graphics.rectangle("fill", no_button_position[0], no_button_position[1], 288, 50)
+	
+	love.graphics.print({{0, 0, 0, 1}, "Sim"}, 163, 382)
+	love.graphics.print({{0, 0, 0, 1}, "Não"}, no_button_position[0] + 118, no_button_position[1] + 12)
+
+	love.graphics.setColor(1, 1, 1)
+end
+
+
+function love.mousemoved(x, y, dx, dy, istouch)
+	if x > no_button_position[0] and x < no_button_position[0] + 288 and y > no_button_position[1] and y < no_button_position[1] + 50 then
+		no_button_position[0] = math.random(0, love.graphics.getWidth() - 288)
+		no_button_position[1] = math.random(0, love.graphics.getHeight() - 50)
+		return
+	end
+
+	if x > 609 - 10 and x < 609 + (close_button:getWidth() * .07) + 10 and y > 20 - 10 and y < 20 + (close_button:getHeight() * .07) + 10 then
+		love.mouse.setCursor(love.mouse.getSystemCursor("hand"))
+		return
+	end
+
+	if x > 36 and x < 36 + 288 and y > 370 and y < 370 + 50 then
+		love.mouse.setCursor(love.mouse.getSystemCursor("hand"))
+		return
+	end
+
+	love.mouse.setCursor(love.mouse.getSystemCursor("arrow"))
+end
+
+
+function love.mousepressed(x, y, k, istouch)
+	if k == 1 then
+		if x > 609 - 10 and x < 609 + (close_button:getWidth() * .07) + 10 and y > 20 - 10 and y < 20 + (close_button:getHeight() * .07) + 10 then love.event.quit() end
+	end
+
+	if x > 36 and x < 36 + 288 and y > 370 and y < 370 + 50 then
+		print('Click')
+	end
 end
